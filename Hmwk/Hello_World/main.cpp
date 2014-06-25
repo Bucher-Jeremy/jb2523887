048f962c843b686618f30dc70b1419dfe3ade2fd
Bucher-Jeremy <jdbucher@live.com>
2014-06-24 20:05:34 -0700

    Signed-off-by: Bucher-Jeremy <jdbucher@live.com>

---
+++ Hmwk/Hello_World/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Jeremy Bucher
+ * Created on June 23, 2014, 9:23 PM
+ * Purpose: First Program
+ */
+
+//System Level Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+//Output simple text
+    cout <<"Hello World"<< endl;
+    //Exit Stage right!
+    return 0;
+}
+

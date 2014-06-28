bfc7433f69fabc46896c0588267a33aca455919a
Bucher-Jeremy <jdbucher@live.com>
2014-06-28 02:28:49 -0700

    Signed-off-by: Bucher-Jeremy <jdbucher@live.com>

--- Hmwk/Assignment 2/Savitch_8thEd_Chap2_Prob2/main.cpp
+++ Hmwk/Assignment 2/Savitch_8thEd_Chap2_Prob2/main.cpp
@@ -16,20 +16,28 @@
 using namespace std;
 
 int main( ) {
-    unsigned short end;
-    while (end > 0)
-    {    
-    end = 0;
-    float wtD, wtM, amtKm, amtKd, contn;
+    //unsigned short end;
+    //while (end > 0)
+    //{    
+    //end = 0;
+    float wtD, wtM, amtKm, amtKd, contn, popdrkn;
     //float const contn;
     contn = 1e-3;
     cout <<"Enter: amount of sweetener need to kill mouse, weight of the mouse, and the weight the dieter will stop dieting\n";
-    cin >>amtK>>wtM>>wtD;
-    amtKd = (wtD * amtKm)/wtM
-    cout <<"Press a number less than 65536 to continue with another calculation or 0 to end the program\n";
-    cin >> end;
+    cin >>amtKm>>wtM>>wtD;
+    amtKd = (wtD * amtKm)/wtM;
+    popdrkn = amtKd / contn;
+    cout <<"The amount of soda pop the dieter can drink is:\n";
+    //cout <<"  \n";
+    cout << popdrkn;
+    //cout <<"Press a number less than 65536 to continue with another calculation or 0 to end the program\n";
+    //cin >> end;
     cout <<"  \n";
-    }
+   // }
     return 0;
 }
 
+//contn * popconsumed = poison consumed
+//pop consumed = poison consumed/contn
+//poison consumed = amtKd
+//pop consumed = amtKd / contn

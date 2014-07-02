ab1497bff5aa50a0fd5b2ccee4aeb2f0f8961164
Bucher-Jeremy <jdbucher@live.com>
2014-07-02 00:11:22 -0700

    Signed-off-by: Bucher-Jeremy <jdbucher@live.com>

---
+++ Hmwk/Assignment 2/Savitch_8thEd_Chap2_Prob13/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   main.cpp
+ * Author: Jeremy Bucher
+ * Created on July 1, 2014, 11:26 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+const short hrToMin=60; //1hr=60min
+const short minToSec=60; //1min=60sec
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    
+    //Input
+    float mph;
+    
+    //Outputs
+    short pacemin,pacesec;
+    
+    cout<<"Enter speed in MPH."<<endl;
+    cin>>mph;
+    short hdthmph;
+    hdthmph=mph/100;
+    pacemin=hrToMin/mph;
+    pacesec=((hrToMin%hdthmph)*100)*minToSec;
+    
+    cout<<"Your pace is "<<pacemin<<"minutes and "<<pacesec<<" seconds per mile.";
+    
+
+    return 0;
+}
+

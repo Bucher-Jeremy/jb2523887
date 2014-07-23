c282bc102a8c9ba8982c05ddcb0f95e010e11ed8
Bucher-Jeremy <jdbucher@live.com>
2014-07-23 02:10:45 -0700

    Signed-off-by: Bucher-Jeremy <jdbucher@live.com>

--- Projects/Project 1/Wheel of Fortune/main.cpp
+++ Projects/Project 1/Wheel of Fortune/main.cpp
@@ -1,68 +1,50 @@
 /* 
  * File:   main.cpp
- * Author: Jeremy
- *
+ * Author: Bucher Jeremy
  * Created on July 20, 2014, 4:02 PM
  */
+
+//System Libraries
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <string>
-
 using namespace std;
-int main(int argc, char** argv) {
-    
-    
-    //Declare variables
-    char round=1, randNum, guess, vowel, count=0,mill=0,halfcar=0, shpree=0, mtrhm=0, trip=0;
-    short wedge, rndTot, topprz=0; //round total
-    int winngs; //winnings
-    char a='*',s='*',h='*',p='*',y='*',c='*',l='*',m='*';
 
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Starts Here
+int main(int argc, char** argv) {
+     
+    //Declare variables
+    char randNum, guess, vowel, phrse, count=1, halfcar=0, shpree=0, mtrhm=0, trip=0;
+    short wedge, winngs=0, rndTot=0; 
+    char a='*',s='*',h='*',p='*',y='*',c='*',l='*',m='*'; //initialize letters of the phrase to asterisks
+    string as="AS", happy="HAPPY", a_="A", clam="CLAM";
+    string _as,_happy,_a,_clam;
     
     //Welcome Statement
     cout<<"Welcome to the Wheel of Fortune"<<endl;
     
-    /*
-      
-      1=Top dollar
-      2=900
-      3=800
-      4=700
-      5=600
-      6=500
-      7=400
-      8=300
-      9=850
-      10=750
-      11=650
-      12=550
-      13=450
-      14=350
-      15=1/2 car
-      16=1/2car
-      17=250
-      18=bankrupt
-      19=bankrupt ==4
-      20=bankrupt
-      21=gift
-      22=prize
-      23=wild
-      24=mill
-     */
+    //Introduce category and output the phrase in asterisks
     cout<<"Round One!"<<endl
-       <<"Your category is: "<<endl
-       <<"Phrases"<<endl
+       <<"Your category is: Phrases."<<endl
+       <<"You have 12 tries to guess the letters that make up the phrase."<<endl
+       <<a<<s<<" "<<h<<a<<p<<p<<y<<" "<<a<<s<<" "<<a<<" "<<c<<l<<a<<m<<endl
+       <<"Lets spin the wheel."<<endl
        <<endl;
-       
-    
 
-    while(count<7){
-        
-       //Set the random seed
+    //do while loop to limit the # of tries to 12
+    do{
+       //Set the random seed and get random number
        srand(static_cast<unsigned int>(time(0)));
        randNum=rand()%24+1;
 
+       //Ternary operator to determine value of the wedge based on the random number received
        wedge=(randNum==2)?900:
              (randNum==3)?800:
              (randNum==4)?700:
@@ -76,20 +58,20 @@ int main(int argc, char** argv) {
              (randNum==12)?550:
              (randNum==13)?450:
              (randNum==14)?350:
-             (randNum==1)?0:
-             (randNum==15)?0:  
-             (randNum==16)?0:
+             (randNum==1)?1500:
              (randNum==17)?250:
-             (randNum==21)?0:
-             (randNum==22)?0:
              (randNum==23)?200:0;
+           
+             
 
-       if(randNum!=18||randNum!=19||randNum!=20){ //18 19 & 20 are bankruptcies 
+       //if the wedge was not a bankruptcy 
+       if(randNum!=18||randNum!=19||randNum!=20){               //18 19 & 20 are bankruptcies 
+           
+           //these are the wedges that are assigned a monetary value
            if(randNum>=2&&randNum<=14||randNum==17||randNum==23){
                cout<<"You spun the $"<<wedge<<" wedge!"<<endl;
            }
            else if(randNum==1){
-               topprz++;
                cout<<"You got the Top Prize Wedge!! It's worth $1500!"<<endl;
            }
            else if(randNum==15||randNum==16){
@@ -103,110 +85,151 @@ int main(int argc, char** argv) {
            else if(randNum==22){ 
                mtrhm++;
                cout<<"You landed on the Motor Home Wedge!! "<<endl;
-           }               
+           } 
 
-           //Obtain Input
+           //input consonant guess
            cout<<"You may now guess an uppercase consonant."<<endl;
            cin>>guess;          
            
-           //buying a vowel
+           //buying a vowel if it hasn't already been bought
            if(a!='A'){
-               cout<<"Do you want to buy a vowel? (Yes/No)"<<endl;
+               cout<<"Do you want to buy a vowel for $250? (Y/N)"<<endl;
                cin>>vowel;
                if(vowel=='Y'){
                    rndTot-=250;
                    a='A';
                }
            }
+           
            //Determine if guess was correct and award money
            if(guess=='S'){
-               //if(randNum)
                cout<<"S occurs two times and you got $";
                winngs=wedge*2;
                cout<<winngs<<" congratulations!"<<endl;
                s='S';
-               count++;
            }
            else if (guess=='H'){
                cout<<"H occurs one time and you got $";
                winngs=wedge;
                cout<<winngs<<" congratulations!"<<endl;
                h='H';
-               count++;
            }
            else if (guess=='P'){
                cout<<"P occurs two times and you got $";
                winngs=wedge*2;
                cout<<winngs<<" congratulations!"<<endl;
                p='P';
-               count++;
            }
            else if (guess=='Y'){
                cout<<"Y occurs one time and you got $";
                winngs=wedge;
                cout<<winngs<<" congratulations!"<<endl;
                y='Y';
-               count++;
            }
            else if (guess=='C'){
                cout<<"C occurs one time and you got $";
                winngs=wedge;
                cout<<winngs<<" congratulations!"<<endl;
                c='C';
-               count++;
            }
            else if (guess=='L'){
                cout<<"L occurs one time and you got $";
                winngs=wedge;
                cout<<winngs<<" congratulations!"<<endl;
                l='L';
-               count++;
            }
            else if (guess=='M'){
                cout<<"M occurs one time and you got $";
                winngs=wedge;
                cout<<winngs<<" congratulations!"<<endl;
                m='M';
-               count++;
            }
            else{
                cout<<"Your guess was unsuccessful."<<endl;
-           }
-           
+               winngs=0;
+           } 
+           count++;
        }
+       //if the wedge was a bankruptcy
        else{
            rndTot=0;
            winngs=0;
            cout<<"You spun a Bankruptcy Wedge"<<endl;
        }
+       rndTot+=winngs;
        
+       //output the phrase with asterisks replaced by the letters that have been bought or guessed
        cout<<a<<s<<" "<<h<<a<<p<<p<<y<<" "<<a<<s<<" "<<a<<" "<<c<<l<<a<<m<<endl
        <<endl;
-            
-       rndTot+=winngs;
-       cout<<"You may now spin again."<<endl
-           <<endl;
-    } 
-    cout<<"Your total winnings for this round is $"<<rndTot<<"."<<endl;
+       
+       cout<<"Would you like to guess the phrase? (Y/N)"<<endl;
+       cin>>phrse;
+       
+       //if user wants to guess phrase 
+       if (phrse=='Y'){
+           //input phrase
+           cout<<"Type the phrase is all capital letters."<<endl;
+           cin>>_as; 
+           cin>>_happy;
+           cin>>_as;
+           cin>>_a;
+           cin>>_clam;
+           //if the input is correct the user wins
+           if(as==_as&&happy==_happy&&a_==_a&&clam==_clam){
+               cout<<"Thats correct!!! You win the round."<<endl;
+               //if the user guesses the correct phrase before obtaining 4 letters they get 20000
+               if (count<=4){
+                   cout<<"You get  20,000 dollars!"<<endl;
+                   rndTot+=20000;
+               }
+               //otherwise they get 7000
+               else{
+                   cout<<"You get 7,000 dollars"<<endl;
+                   rndTot+=7000;
+               }
+               //increment counter by 12 to escape the letter guessing loop
+               count+=12;
+           }
+           //if the input is false the user continues guessing and losses 1000 dollars
+           else{
+               cout<<"Thats incorrect."<<endl;
+               rndTot-=1000;
+               rndTot+=winngs;
+               cout<<"Your total winnings for this round is $"<<rndTot<<"."<<endl;
+               cout<<"You may now spin again."<<endl;
+           }
+       }
+       //if they don't want to guess the phrase they continue guessing letters
+       else{
+           rndTot+=winngs;
+           cout<<"Your total winnings for this round is $"<<rndTot<<"."<<endl;
+           cout<<"You may now spin again."<<endl;
+       }
+       
+       //if all the letters are revealed the game is over 
+       if(a=='A'&&s=='S'&&h=='H'&&p=='P'&&y=='Y'&&c=='C'&&l=='L'&&m=='M'){
+           count+=4; //If all the letters of the phrase are acquired, the counter is 8, so add 4 to jump from the letter guessing loop
+       }
+    }while(count<12); 
+    
+    //output the amount the user won
+    cout<<"You won $"<<rndTot<<"!"<<endl;
+    
+    //If they won something other than money output that as well
     if(halfcar>=2){
        cout<<"You also won a brand new car!!"<<endl;
     }
-    else if(shpree>0){
+    if(shpree>0){
        cout<<"Congratulations on your $1,000 Shopping Spree!!"<<endl;
     }
-    else if(mtrhm>0){
+    if(mtrhm>0){
        cout<<"You also won a new motor home."<<endl;
     }
-    else if(trip>0){
+    if(trip>0){
        cout<<"Congratulations on your paid trip to a destination of your choice!"<<endl;
-    }
-
-    cout<<"Thats correct! The phrase is:"<<endl
-       <<"AS HAPPY AS A CLAM"<<endl;
-
-    //cout<<noCshPz;
- 
+    } 
 
     return 0;
 }
+
 
--- Projects/Project 1/Wheel of Fortune/nbproject/Makefile-impl.mk
+++ Projects/Project 1/Wheel of Fortune/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Wheel of Fortune
+PROJECTNAME=wheel of fortune
 
 # Active Configuration
 DEFAULTCONF=Debug
